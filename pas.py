4ecad3f349e19a7c90d9f8f5e23be19c375bbaf1
Candee <thang.nb153505@sis.hust.edu.vn>
2023-02-09 15:55:33 +0700

    hide console with ctypes

--- pas.py
+++ pas.py
@@ -1,13 +1,12 @@
 from tendo import singleton
 import pyautogui
 import webview
-import win32gui
-import win32con
+# import win32gui
+# import win32con
 import pygetwindow as gw
-
+import ctypes
 
 pw = gw.getWindowsWithTitle('pas')
-print(len(pw))
 
 
 def pasWindow():
@@ -15,9 +14,18 @@ def pasWindow():
     if len(pw) > 0:
         for p in pw:
             try:
+                p.activate()
                 p.maximize()
             except:
                 pass
+
+
+def hide_console():
+    kernel32 = ctypes.WinDLL('kernel32')
+    user32 = ctypes.WinDLL('user32')
+    SW_HIDE = 0
+    hWnd = kernel32.GetConsoleWindow()
+    user32.ShowWindow(hWnd, SW_HIDE)
 
 
 # độ phân giải màn hình
@@ -30,14 +38,16 @@ me = singleton.SingleInstance()
 
 
 # ẩn tiến trình với tên pas.exe
-try:
-    frgrnd_wndw = win32gui.GetForegroundWindow()
-    wndw_title = win32gui.GetWindowText(frgrnd_wndw)
-    if wndw_title.endswith("pas.exe"):
-        win32gui.ShowWindow(frgrnd_wndw, win32con.SW_HIDE)
-except:
-    pass
+# try:
+#     frgrnd_wndw = win32gui.GetForegroundWindow()
+#     wndw_title = win32gui.GetWindowText(frgrnd_wndw)
+#     if wndw_title.endswith("pas.exe"):
+#         win32gui.ShowWindow(frgrnd_wndw, win32con.SW_HIDE)
+# except:
+#     pass
 
+
+hide_console()
 
 # hiển thị dưới dạng Webview
 window = webview.create_window(
